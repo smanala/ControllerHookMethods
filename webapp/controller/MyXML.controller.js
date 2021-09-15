ed7ac74678062fe140fcba1f46a048185cdcb18d
smanala <saranyam.in@mouritech.com>
2021-09-15 14:17:15 +0000

    first version

---
+++ webapp/controller/MyXML.controller.js
@@ -0,0 +1,35 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function(Controller) {
+	'use strict';
+	return Controller.extend("masha.controller.MyXML", {
+		onInit: function() {
+			console.log("Child Class Constructor");
+			//a global variable @ class level which can be used by other methods
+			this.oView = this.getView();
+		},
+		onBeforeRendering: function() {
+			//if(checkings....)
+			this.oView.byId("MyButton").setEnabled(false);
+		},
+		onAfterRendering: function() {
+			$("#myView--myForm").hide().fadeIn(5000);
+		},
+		//oView : this.getView(),
+		clickMe: function(params) {
+			var oControl = this.oView.byId("MyButton");
+			oControl.setVisible(false);
+		},
+		onShow: function() {
+			var oEmpId = this.oView.byId("idEmpID");
+			console.log("Employee ID is : ", oEmpId.getValue());
+
+			var oEmpName = this.oView.byId("idEmpName");
+			console.log("Employee Name is : ", oEmpName.getValue());
+
+			var oSal = this.oView.byId("idEmpSal");
+			console.log("Employee Salary is : ", oSal.getValue());
+			console.log("Employee Currency is : ", this.oView.byId("idEmpCurr").getValue());
+		}
+	});
+});
